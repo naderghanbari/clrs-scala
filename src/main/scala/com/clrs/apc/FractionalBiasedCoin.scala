401b00a2b1348558a4b7789100fbc7a191a83746
Nader Ghanbari <nader.ghanbari@yoppworks.com>
2019-05-14 22:25:09 -0400

    minor reformat and docs

--- src/main/scala/com/clrs/apc/FractionalBiasedCoin.scala
+++ src/main/scala/com/clrs/apc/FractionalBiasedCoin.scala
@@ -1,6 +1,10 @@
 package com.clrs.apc
 
-/** Given two numbers `a` and `b`, such that `a < b`, simulates a biased coin such that
+import scala.Function.tupled
+
+/** Appendix C, Exercise C.2-6, Page 1195
+  *
+  * Given two numbers `a` and `b`, such that `a < b`, simulates a biased coin such that
   * the probability of `Heads` (represented by True) is `a/b`.
   *
   * Does so by first creating a (possibly infinite) stream of bits in the binary
@@ -9,7 +13,10 @@ package com.clrs.apc
 class FractionalBiasedCoin(a: Long, b: Long) extends Coin {
   require(0 < a && a < b, "0 < a < b")
 
-  val bits   = LazyList.iterate(a)(_*2 % b).takeWhile(0.!=).map(_*2 >= b)
-  def fairs  = Iterator.continually(FairCoin.toss())
-  def toss() = bits.zip(fairs).find(Function.tupled(_==_)).exists(_._1)
+  private val bits = LazyList.iterate(a)(_ * 2 % b).takeWhile(0.!=).map(_ * 2 >= b)
+
+  private def fairs = Iterator.continually(FairCoin.toss())
+
+  override def toss(): Boolean = bits.zip(fairs).find(tupled(_ == _)).exists(_._1)
+
 }
