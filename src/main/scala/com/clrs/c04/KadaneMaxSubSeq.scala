715f1010e757ded18f5baea86b562930c3dff6a1
Nader Ghanbari <nader.ghanbari@yoppworks.com>
2019-06-15 14:38:28 -0400

    Kadane's algorithm (minor change)

--- src/main/scala/com/clrs/c04/KadaneMaxSubSeq.scala
+++ src/main/scala/com/clrs/c04/KadaneMaxSubSeq.scala
@@ -11,7 +11,7 @@ object KadaneMaxSubSeq extends MaxSubSeq {
   def maxSubSeq[T: Numeric](a: Arr[T]): Option[SubSequence[T]] = {
     val zero = implicitly[Numeric[T]].zero
     a.view.zipWithIndex
-      .scanLeft(SubSequence(0, 0, zero)) {
+      .scanLeft(SubSequence(-1, -1, zero)) {
         case (p, (x, i)) if p.sum > zero => SubSequence(p.start, i, p.sum + x)
         case (_, (x, i))                 => SubSequence(i, i, x)
       }
